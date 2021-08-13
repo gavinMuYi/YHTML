1d1ad7663e4df5260dbfa9c1d9b906743a1edc9b
gavinjhyang <gavinjhyang@tencent.com>
2021-08-13 17:56:52 +0800

    example

--- src/components/pagination/src/index.vue
+++ src/components/pagination/src/index.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="y-pagination">
-        <div>共 {{ total }} 项，每页显示 {{ count }}</div>
-        <div>{{ limit }}</div>
+        <div class="total-count">共 {{ total }} 项，每页显示 {{ count }}</div>
+        <div class="page-nums">
+            <div v-for="num in limit" :key="num" class="nums">{{ num }}</div>
+        </div>
     </div>
 </template>
 
@@ -15,15 +17,16 @@ export default {
         },
         index: {
             type: Number,
-            default: 0
+            default: 1
         },
         count: {
             type: Number,
-            default: 0
+            default: 15
         }
     },
     data() {
         return {
+            currentIndex: this.index,
         };
     },
     computed: {
@@ -38,6 +41,25 @@ export default {
 
 <style lang="less">
     .y-pagination {
-
+        display: flex;
+        .total-count {
+            font-size: 14px;
+            display: flex;
+            align-items: center;
+        }
+        .page-nums {
+            display: flex;
+            flex: 1;
+            justify-content: flex-end;
+            .nums {
+                display: flex;
+                margin: 0 5px;
+                width: 20px;
+                height: 30px;
+                align-items: center;
+                justify-content: center;
+                font-size: 12px;
+            }
+        }
     }
 </style>
