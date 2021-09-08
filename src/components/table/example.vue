8708a4f8cf6b5eb92b2b161fe0f3a5f61fe52b0f
gavinjhyang <gavinjhyang@tencent.com>
2021-09-08 16:40:06 +0800

    table fixed

--- src/components/table/example.vue
+++ src/components/table/example.vue
@@ -1,20 +1,21 @@
 <template>
     <div class="y-table_example">
         <y-table :count="2" :multiple="true" :contentMaxHeight="250">
-            <y-table-column label="keyLabelCenter" columnKey="key">
-                <y-table-column label="keyLabelCenter-1-3214120394i12904i129034" columnKey="key">
+            <y-table-column label="keyLabelLeft" columnKey="key" fixed="left">
+                <y-table-column label="keyLabel-3214120394i12904i129034" columnKey="key">
                 </y-table-column>
             </y-table-column>
-            <y-table-column label="keyLabelCenter2" columnKey="key" width="250px" />
             <y-table-column label="keyLabelRight" columnKey="key" fixed="right" width="200px" />
-            <y-table-column label="一级" fixed="left">
+            <y-table-column label="一级">
                 <div slot="header">header-111-一级</div>
                 <y-table-column label="二级" columnKey="label" width="50px">
                 </y-table-column>
                 <y-table-column label="二级">
-                    <y-table-column label="三级" columnKey="label" width="20%">
-                        <div slot="header">cell slot</div>
-                        <div slot-scope="props" slot="cell">{{ props.label + '-' + props.key }}</div>
+                    <y-table-column label="三级" columnKey="label" width="100px">
+                        <div slot="header">cell slot can click</div>
+                        <div slot-scope="props" slot="cell" @click="heightadd">
+                            {{ props.label + '-' + props.key + str }}
+                        </div>
                     </y-table-column>
                     <y-table-column label="三级" columnKey="label" width="50px">
                         <div slot="header">header-111-三级</div>
@@ -25,6 +26,7 @@
                 <y-table-column label="Labelin1" columnKey="label" width="100px" />
                 <y-table-column label="Labelin2" columnKey="label" width="50px" />
             </y-table-column>
+            <y-table-column label="keyLabelCenter" columnKey="key" width="550px" />
         </y-table>
         <!-- <div class="ex-table-box" ref="box">
             <table class="ex-table">
@@ -53,23 +55,18 @@ export default {
     },
     data() {
         return {
-            left: 0
+            left: 0,
+            str: ''
         };
     },
-    mounted() {
-        // let element = this.$refs.box;
-        // element.addEventListener('scroll', () => {
-        //     this.left = element.scrollLeft;
-        //     console.log('11221', element.scrollLeft, this.left);
-        // });
-        // console.log(element);
-        // let callback = () => {
-        //     console.log('111');
-        // };
-        // let observer = new MutationObserver(callback);
-        // observer.observe(element, { attributes: true, attributeFilter: ['style'], attributeOldValue: true });
-    },
     methods: {
+        heightadd() {
+            if (!this.str) {
+                this.str = 'sahdvhavjdskjvknasjkvbajksdbvnjkasdvjasdv';
+            } else {
+                this.str = '';
+            }
+        }
     }
 };
 </script>
